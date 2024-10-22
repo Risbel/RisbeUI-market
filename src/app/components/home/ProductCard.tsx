50fbf30a1ab337577dd46089ec68b042b96b5de1
Risbel Suarez <risbel961019@gmail.com>
2024-10-22 19:25:58 -0400

    feat: Add NewestProducts section

---
+++ src/app/components/home/ProductCard.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import Image from "next/image";
+import Link from "next/link";
+
+interface iAppProps {
+  images: string[];
+  name: string;
+  price: number;
+  smallDescription: string;
+  id: string;
+}
+
+const ProductCard = ({ images, name, price, smallDescription, id }: iAppProps) => {
+  return (
+    <div className="rounded-lg">
+      <Carousel className="w-full mx-auto">
+        <CarouselContent>
+          {images.map((image) => {
+            return (
+              <CarouselItem>
+                <div className="relative h-[230px]">
+                  <Image alt="product image" src={image} fill className="object-cover w-full h-full rounded-lg" />
+                </div>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious className="ml-14" />
+        <CarouselNext className="mr-14" />
+      </Carousel>
+
+      <div className="flex justify-between items-center mt-2">
+        <h1>{name}</h1>
+        <h3>${price}</h3>
+      </div>
+      <p className="text-gray-600 line-clamp-2">{smallDescription}</p>
+
+      <Button asChild className="w-full mt-5">
+        <Link href={`/product/${id}`}>More details</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import NewestProducts from "./components/home/NewestProducts";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <section className="max-w-7xl mx-auto px-4 md:px-8">
+      <div className="max-w-3xl mx-auto text-2xl md:text-5xl lg:text-6xl font-semibold text-center">
+        <h1>Find the best Tailwind</h1>
+        <h1 className="text-primary">Template & Icons</h1>
+        <p className="lg:text-lg text-muted-foreground mx-auto mt-5 w-[90%] font-normal text-base">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, id eligendi nihil natus voluptates ipsum
+          laboriosam maxime. Sint fugit officiis excepturi.
+        </p>
+      </div>
+
+      <NewestProducts />
+    </section>
+  );
 }
